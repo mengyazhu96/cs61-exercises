4f0989023fee875dce5f24d4c88ddd747464828c
Jessica Zhu <jessicazhu@college.harvard.edu>
2016-09-24 03:18:29 +0000

    Finish asm2x 2a

--- asm1x/mystery3.c
+++ asm1x/mystery3.c
@@ -6,7 +6,12 @@ int op2(int arg1, int arg2) {
 	return ~arg1 | arg2;
 }
 
-int mystery3(void) {
-	const char* msg = "It worked!";
-	return 4;
-}
+const char* mystery3 = "It worked";
+// const char* msg = "It worked";
+// void mystery3() {
+// 	(void) msg;
+// }
+// int mystery3(void) {
+// 	const char* msg = "It worked!";
+// 	return 4;
+// }
---
+++ asm2x/2a.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void output_func(int* arg1, int arg2) {
+	printf("%i\n", arg1[arg2]);
+}
