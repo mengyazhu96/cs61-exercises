933fe1e7cc6a9cb609d5604fae0fe9f42f9f4fb7
Jessica Zhu <jessicazhu@college.harvard.edu>
2016-09-24 03:21:47 +0000

    Finish asm2x 2

---
+++ asm2x/2b.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void output_func(char* arg1, int arg2) {
+	printf("%c\n", arg1[arg2]);
+}
---
+++ asm2x/2c.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void output_func(long* arg1, int arg2) {
+	printf("%ld\n", arg1[arg2]);
+}
